aebc66aafe71c8d4273752fb7a3da2788ad06935
ArulA21 <arul@telecmi.com>
2025-04-23 18:27:11 +0530

    experience updated

--- src/About.js
+++ src/About.js
@@ -9,7 +9,7 @@ const About = () => {
             <div className='md:w-1/2'>
                 <Lottie loop={true} animationData={about} className='pb-5'/>
             </div>
-            <div className='md:w-1/2 flex flex-col justify-center'>
+            {/* <div className='md:w-1/2 flex flex-col justify-center'>
                 <div className='flex flex-col justify-center mb-3' >
                     <h1 className='text-4xl border-b-4 border-primary mb-5 w-[170px] font-bold'>About Me</h1>
                     <p>Hi, I’m Arul, a dedicated and motivated professional transitioning from a successful career in the BPO industry to pursue my passion for web development.</p>
@@ -19,6 +19,19 @@ const About = () => {
                     <h1 className='text-4xl border-b-4 border-primary mb-5 w-[220px] font-bold'>My Journey</h1>
                     <p>After 3 years in the BPO sector, where I honed my skills in communication, project management, and client satisfaction, I decided to follow my interest in web development. </p>
                     <p className='pt-5'>I have since dedicated my time to learning and mastering front-end technologies, with a particular focus on React js (Vite), JavaScript, HTML, and CSS (Tailwind css & Bootstrap).</p>
+                </div>
+            </div> */}
+
+            <div className='md:w-1/2 flex flex-col justify-center'>
+                <div className='flex flex-col justify-center mb-3'>
+                    <h1 className='text-4xl border-b-4 border-primary mb-5 w-[170px] font-bold'>About Me</h1>
+                    <p>Hi, I’m Arul, a passionate Front-End Developer with a strong foundation in building dynamic and responsive web applications.</p>
+                    <p className='pt-5'>Currently working at TeleCMI Communications Pvt Ltd, I specialize in creating seamless user experiences using React, Vue.js, Tailwind CSS, and Express.js.</p>
+                </div>
+                <div className='flex flex-col justify-center'>
+                    <h1 className='text-4xl border-b-4 border-primary mb-5 w-[220px] font-bold'>My Journey</h1>
+                    <p>I began my professional career in the BPO industry, where I developed essential skills in communication, data accuracy, and team leadership.</p>
+                    <p className='pt-5'>Motivated to pursue my passion for tech, I transitioned into web development and have since worked on production-level projects involving custom blog CMS, API integration, and responsive UI development using modern front-end frameworks.</p>
                 </div>
             </div>
 
